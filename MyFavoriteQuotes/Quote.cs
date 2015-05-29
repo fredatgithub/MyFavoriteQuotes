fb44c73f74414fe2df53b1ca7bd959c5b10fcdc1
Freddy Juhel <domaine75@gmail.com>
2015-05-29 12:54:01 +0200

    Add Quote and Quotes classes

---
+++ MyFavoriteQuotes/Quote.cs
@@ -0,0 +1,16 @@
+﻿namespace MyFavoriteQuotes
+{
+  class Quote
+  {
+    public string Author { get; set; }
+    public string Language { get; set; }
+    public string Sentence { get; set; }
+
+    public Quote(string author = "unknown author", string language = "English", string sentence = "")
+    {
+      Author = author;
+      Language = language;
+      Sentence = sentence;
+    }
+  }
+}
---
+++ MyFavoriteQuotes/Quotes.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace MyFavoriteQuotes
+{
+  class Quotes
+  {
+    public List<Quote> ListOfQuotes { get; set; }
+
+    public Quotes()
+    {
+      ListOfQuotes = new List<Quote>();
+    }
+
+    public void Add(Quote quote)
+    {
+      ListOfQuotes.Add(quote);
+    }
+  }
+}

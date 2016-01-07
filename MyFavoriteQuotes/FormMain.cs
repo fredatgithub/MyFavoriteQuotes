c36888d52c13ce8990ce055339b8cbef7607ce4d
fred <domaine75@gmail.com>
2016-01-08 00:16:19 +0100

    Update FormMain.cs

--- MyFavoriteQuotes/FormMain.cs
+++ MyFavoriteQuotes/FormMain.cs
@@ -1864,7 +1864,7 @@ namespace MyFavoriteQuotes
     {
       if (tb != ActiveControl) return;
       var selectionIndex = tb.SelectionStart;
-      tb.Text = tb.Text.Insert(selectionIndex, Clipboard.GetText());
+      tb.SelectedText = Clipboard.GetText();
       tb.SelectionStart = selectionIndex + Clipboard.GetText().Length;
     }
 

17ba358206df77e44914c93039c687fa610d2a46
Freddy Juhel <domaine75@gmail.com>
2017-09-18 15:06:03 +0200

    Add one project to split one big xml file into several small ones

---
+++ SplitXmlFile/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace SplitXmlFile
+{
+  internal static class Program
+  {
+    private static void Main()
+    {
+      Action<string> display = Console.WriteLine;
+      display("séparation d'un gros fichier xml en plusieurs petit");
+
+      display("Press any key to exit:");
+      Console.ReadKey();
+    }
+  }
+}

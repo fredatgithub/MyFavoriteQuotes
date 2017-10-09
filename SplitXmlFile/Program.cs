198f3b511a8a648b660fe10295565ec0e2a5286c
Fred <domaine75@gmail.com>
2017-10-09 13:54:28 +0200

    remove useless quote template

--- SplitXmlFile/Program.cs
+++ SplitXmlFile/Program.cs
@@ -11,10 +11,13 @@ namespace SplitXmlFile
     private static void Main()
     {
       Action<string> display = Console.WriteLine;
-      display("séparation d'un gros fichier xml en plusieurs petit");
+      display("séparation d'un gros fichier xml en plusieurs petits");
       string fileName = "Quote_files\\quote1.xml";
       int startNumber = 3;
-      
+      int numberOfQuotePerFile = 250;
+      string xmlHeader = @"<?xml version=""1.0"" encoding=""utf-8"" ?>";
+
+
       display("Press any key to exit:");
       Console.ReadKey();
     }
